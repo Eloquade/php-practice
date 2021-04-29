99ecd22dcfbf8c076443c902db3184825223d50a
Jheave <jimenezjheave123@gmail.com>
2021-04-29 10:06:46 +0800

    fix html file wrong format

--- triangle.php
+++ triangle.php
@@ -1,48 +1,42 @@
 <html>
-<head>
-<title>PHP Program To determine the area of triangle</title>
-</head>
-<body>
-<form method="post">
-<table border="0">
-<tr>
-<td> <input type="text" name="num1" value="" placeholder="Enter a value"/> </td>
-</tr>
-<tr>
-<td> <input type="text" name="num2" value="" placeholder="Enter b value"/> </td>
-</tr>
-<tr>
-<td> <input type="text" name="num3" value="" placeholder="Enter c value"/> </td>
-</tr>
+  <head>
+    <title>PHP Program To determine the area of triangle</title>
+  </head>
+  <body>
+    <form method="post">
+      <table border="0">
+      <tr>
+        <td> <input type="text" name="num1" value="" placeholder="Enter a value"/> </td>
+      </tr>
+      <tr>
+        <td> <input type="text" name="num2" value="" placeholder="Enter b value"/> </td>
+      </tr>
+      <tr>
+        <td> <input type="text" name="num3" value="" placeholder="Enter c value"/> </td>
+      </tr>
 
-<tr>
-<td> <input type="submit" name="submit" value="Submit"/>
-</td>
-</tr>
+      <tr>
+        <td> <input type="submit" name="submit" value="Submit"/>
+      </td>
+      </tr>
 
-</table>
-</form>
-<?php
-$x='submit';
-do{
-if(isset($_POST['submit']))
-{
-$a = $_POST['num1'];
-$b = $_POST['num2'];
-$c = $_POST['num3'];
-// Compute s //
-$s = ($a + $b + $c) / 2;
-$area = sqrt($s * ($s - $a) * ($s - $b) * ($s - $c)); //Formula for area of the triangle
-$precision = 4;
-echo " Area of the trainagle is = ".number_format($area, $precision);
-echo "";
-  $x++;
-}
-} while($x=2);
-    {
-        echo"do you want to try again ";
-
-    }
-?>
-</body>
+      </table>
+    </form>
+    <?php
+      $x='submit';
+      
+      if(isset($_POST['submit']))
+      {
+      $a = $_POST['num1'];
+      $b = $_POST['num2'];
+      $c = $_POST['num3'];
+      // Compute s //
+      $s = ($a + $b + $c) / 2;
+      $area = sqrt($s * ($s - $a) * ($s - $b) * ($s - $c)); //Formula for area of the triangle
+      $precision = 4;
+      echo " Area of the trainagle is = ".number_format($area, $precision);
+      echo "";
+    
+    ?>
+  </body>
 </html>
