952ed186c7d0847cb666e37a0773a696f0a7455d
Jheave <jimenezjheave123@gmail.com>
2021-04-29 10:14:29 +0800

    fix html file wrong format

--- circle.php
+++ circle.php
@@ -1,35 +1,34 @@
 <html>
-<head>
-<title>PHP Program To find Area and Circumference of a Circle</title>
-</head>
-<body>
-<form method="post">
-<table border="0">
-<tr>
-<td><input type="text" name="num1" value="" placeholder="Enter the radius of a circle"/></td>
-</tr>
-<tr>
-<td> <input type="submit" name="submit" value="Submit"/></td>
-</tr>
-</table>
-</form>
-<?php
-if(isset($_POST['submit']))
-{
-$r = $_POST['num1'];
-$pi = 3.14;
-$area = $pi * $r * $r;
-echo "Area of a Circle is: ".$area;
-echo "<br>";
-$cir = 2*$pi*$r;
-echo "Circumference of a circle is: " .$cir;
-echo "<br>";
-$diameter = 2*$r;
-echo "diameter of a circle is: " .$cir;
-echo "<br>";
-
-return 0;
-}
-?>
-</body>
+    <head>
+        <title>PHP Program To find Area and Circumference of a Circle</title>
+    </head>
+    <body>
+        <form method="post">
+            <table border="0">
+                <tr>
+                    <td><input type="text" name="num1" value="" placeholder="Enter the radius of a circle"/></td>
+                </tr>
+                <tr>
+                    <td> <input type="submit" name="submit" value="Submit"/></td>
+                </tr>
+            </table>
+        </form>
+        <?php
+        if(isset($_POST['submit'])){
+            $r = $_POST['num1'];
+            $pi = 3.14;
+            $area = $pi * $r * $r;
+            echo "Area of a Circle is: ".$area;
+            echo "<br>";
+            $cir = 2*$pi*$r;
+            echo "Circumference of a circle is: " .$cir;
+            echo "<br>";
+            $diameter = 2*$r;
+            echo "diameter of a circle is: " .$cir;
+            echo "<br>";
+            
+            return 0;
+        }
+        ?>
+    </body>
 </html>
