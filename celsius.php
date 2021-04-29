7df5e1eee93ca41f4281b46d47773cc111632773
Jheave <jimenezjheave123@gmail.com>
2021-04-29 12:42:37 +0800

    added loop

--- celsius.php
+++ celsius.php
@@ -13,36 +13,76 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
    </head>
    <body>
-      <div class="container h-100">
-         <div class="row h-100 justify-content-center align-items-center">
-            <form class="row g-3 needs-validation" method="post" novalidate>
-             <strong><h1>Convert fahrenheit to Celsius</h1></strong>
-              <div class="col-md-4">
-                <label for="validationCustom01" class="form-label">Enter Temprature in Fahrenheit : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                <input type="text" class="form-control" autocomplete="off" id="validationCustom01" required name="far">
-                <div class="invalid-feedback">
-                  Please Enter Temprature in Fahrenheit!
-                </div>
-              </div>
-              <div class="col-md-4">
-                 <label for="validationCustom02" class="form-label">Temprature in Celcius</label>
-                 <input type="text" class="form-control"  id="txtSecond" required disabled>
-               </div>
-              <div class="col-12">
-                <button class="btn btn-primary"name= "submit" type="submit">Convert</button>
-                <button class="btn btn-danger" name= "reset" type="reset">Reset</button>
-              </div>
-
-            </form>
-         </div>   
-      </div>  
       <?php
-         if(isset($_POST['submit'])){
-            $f= $_POST['far'];
-            $c= ($f - 32) * (5/9);
-            echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input class='form-control' id='txtFirst' value=$c >";
-         }
-     ?>
+       session_start();
+
+       if(!isset($_POST['submit']))
+       {
+
+           $_SESSION['count']=0;
+
+       }
+       ?>
+       <div class="container h-100" id="demo">
+            <div class="row h-100 justify-content-center align-items-center">
+               <form class="row g-3 needs-validation" method="POST" novalidate>
+                <strong><h1>Convert fahrenheit to Celsius</h1></strong>
+                 <div class="col-md-4">
+                   <label for="validationCustom01" class="form-label">Enter Temprature in Fahrenheit : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                   <input type="text" class="form-control" autocomplete="off" id="validationCustom01" required name="far">
+                   <div class="invalid-feedback">
+                     Please Enter Temprature in Fahrenheit!
+                   </div>
+                 </div>
+                 <div class="col-md-4">
+                    <label for="validationCustom02" class="form-label">Temprature in Celcius</label>
+                    <input type="text" class="form-control"  id="txtSecond" required disabled>
+                  </div>
+                 <div class="col-12">
+                   <button class="btn btn-primary"name="submit" type="submit">Convert</button>
+                   <button class="btn btn-danger" name= "reset" type="reset">Reset</button>
+                 </div>
+
+               </form>
+            </div>   
+       </div>  
+
+       <?php
+    if(isset($_POST['submit'])){
+          $count=$_SESSION['count']++;
+          $f= $_POST['far'];
+          $c= ($f - 32) * (5/9);
+          $count=$_SESSION['count']++;
+
+          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input class='form-control' id='txtFirst' value=$c >";
+          if($count == 3)
+          {
+              ?>
+                  <div class="container h-100">
+                       <div class="row h-100 justify-content-center align-items-center">
+                          <form class="row g-3 needs-validation" method="POST" novalidate>
+                           <strong><h1>You Enter 2 times</h1></strong>
+                            <div class="col-md-4">
+                              <button class="btn btn-primary"name="ok" type="submit">Continue</button>
+                            </div>
+                          </form>
+                       </div>   
+                  </div>  
+                  <script>
+                     function myFunction() {
+                       var myobj = document.getElementById("demo");
+                       myobj.remove();
+                     }
+                     myFunction();
+                  </script>
+              <?php
+              if(isset($_POST['ok']))
+              {    
+              header("Refresh:0");
+              }
+          }
+      }
+    ?>
      <script>
          document.addEventListener('DOMContentLoaded', () =>{
              if (document.getElementById('txtFirst').value != 0 ){
--- test.php
+++ test.php
@@ -13,23 +13,45 @@
 
     }
     ?>
-    <form action="" method="POST">
-  <i> color </i><input type="text" name="text">
-  <i> color amount </i> <input type="number" name="num">
-  <input type="submit" value="Go" name="submit">
-</form>
+    <div class="container h-100">
+         <div class="row h-100 justify-content-center align-items-center">
+            <form class="row g-3 needs-validation" method="POST" novalidate>
+             <strong><h1>Convert fahrenheit to Celsius</h1></strong>
+              <div class="col-md-4">
+                <label for="validationCustom01" class="form-label">Enter Temprature in Fahrenheit : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                <input type="text" class="form-control" autocomplete="off" id="validationCustom01" required name="far">
+                <div class="invalid-feedback">
+                  Please Enter Temprature in Fahrenheit!
+                </div>
+              </div>
+              <div class="col-md-4">
+                 <label for="validationCustom02" class="form-label">Temprature in Celcius</label>
+                 <input type="text" class="form-control"  id="txtSecond" required disabled>
+               </div>
+              <div class="col-12">
+                <button class="btn btn-primary"name="submit" type="submit">Convert</button>
+                <button class="btn btn-danger" name= "reset" type="reset">Reset</button>
+              </div>
+
+            </form>
+         </div>   
+    </div>  
 
 <?php
     if(isset($_POST['submit']))
     {
-        echo $_POST['text'];
-        echo "<br>";
-        echo $_POST['num'];
+        echo $_POST['far'];
         echo "<br>";
         $count=$_SESSION['count']++;
+        $f= $_POST['far'];
+        $c= ($f - 32) * (5/9);
+        echo "<br>";
+        $count=$_SESSION['count']++;
+
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input class='form-control' id='txtFirst' value=$c >";
         if($count==3)
         {
-            echo "you have received 10 values";
+            echo "You enter 2 times";
             echo "<br>";
             echo "press ok to get again";
             ?>
